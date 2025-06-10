edbf2a97701b1bfb13ce2611312df0949695c162
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2025-06-10 15:43:51 +0800

    Added page not found in about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,11 @@ import { getMarkDown } from '../utils/getMarkdown';
 export default function About() {
   const MarkdownData = getMarkDown();
   const markdown = MarkdownData.find(p => p.slug === "about_me");
+
+  if (!markdown) {
+    return <center><div>Page not found</div></center>
+  }
+
   return (
     <section className="max-w-3xl mx-auto p-6 my-10 bg-white rounded shadow">
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
