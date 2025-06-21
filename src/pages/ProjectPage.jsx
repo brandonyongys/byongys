4d61d6ac60eb2dbc316e77ac70654eeb2fcaed5f
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2025-06-21 12:54:54 +0800

    Updated project page to display similarly to post page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,19 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { getMarkDown } from '../utils/getMarkdown';
 
-export default function ProjectPage() {
+export default function ProjectPostPage() {
   const { slug } = useParams();
-  const projects = getMarkDown('projects');
-  const project = projects.find(p => p.slug === slug);
+  const projects = getMarkDown('projects')
+  const project = projects.find((p) => p.slug === slug);
+
+  // const relatedPosts = blogPosts.filter((post) => post.projectSlug === slug);
 
   if (!project) {
     return <div>Project not found</div>;
   }
 
   return (
-    <article className="max-w-3xl mx-auto py-8 px-4">
-      <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
-      <p className="text-sm text-gray-500 mb-6">{new Date(project.date).toLocaleDateString('en-GB')}</p>
+    <article className="max-w-3xl mx-auto p-8 my-8 bg-gray-50 rounded shadow">
+      <h1 className="text-4xl font-bold mb-4 text-orange-800">{project.title}</h1>
+      {/* <img src={project.image} alt={project.title} className="rounded-xl mb-4" /> */}
+      <p className="text-m font-semibold text-orange-700">{project.description}</p>
+      {/* <p className="text-sm font-semibold text-orange-600 mb-6">First created: {new Date(project.date).toLocaleDateString('en-GB')}</p> */}
+      <p className="text-sm font-semibold text-orange-600 mb-6">Last updated: {new Date(project.updated_date).toLocaleDateString('en-GB')}</p>
       <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: project.content }} />
     </article>
   );
