7d8eb3f1c19197f958c6732b0623a4c4c1cd8093
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2025-06-22 15:42:44 +0800

    Return missing page if no projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,16 @@
 import ProjectCard from "../components/ProjectCard";
 // import blogPosts from "../data/blogPosts.json";
 import { getMarkDown } from '../utils/getMarkdown';
+import MissingPage from '../components/MissingPage';
 
 export default function ProjectsPage() {
   const projects = getMarkDown("projects")
   console.log("projects", projects)
+
+  if (projects.length === 0){
+    return <MissingPage pageName='Projects' />;
+  }
+
   return (
     // <section className="max-w-4xl mx-auto p-8 my-8 bg-gray-50 rounded shadow">
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-2 p-4">
