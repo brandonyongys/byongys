11b789683363ebce967f4aef04df26aeff4e8f11
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2025-06-22 17:27:47 +0800

    Changed all datetime from DD/MM/YYYY to DD MMM YYYY format.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -15,7 +15,13 @@ export default function PostPage() {
     <article className="max-w-3xl mx-auto p-8 my-8 bg-gray-50 rounded shadow">
       <h1 className="text-4xl font-bold mb-4 text-orange-800">{post.title}</h1>
       <p className="text-m font-semibold text-orange-700">{post.description}</p>
-      <p className="text-sm font-semibold text-orange-600 mb-6">{new Date(post.date).toLocaleDateString('en-GB')}</p>
+      <p className="text-sm font-semibold text-orange-600 mb-6">
+        {new Date(post.date).toLocaleDateString('en-GB', {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric',
+        })}
+      </p>
       <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: post.content }} />
     </article>
   );
