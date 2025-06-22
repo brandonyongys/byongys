f1f89d995c6681609ce7f8997ddc5f8627bc9d77
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2025-06-22 15:28:37 +0800

    Recreated MissingPage

---
+++ src/components/MissingPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function MissingPage({pageName}) {
+    return (
+        <article className="max-w-3xl mx-auto p-8 my-8 bg-gray-50 rounded shadow">
+            <h1 className="text-4xl font-bold mb-4 text-orange-800">{pageName} not found</h1>
+            {/* <Link to="/" className="text-blue-600 underline">Go back home</Link> */}
+        </article>
+    )
+}
