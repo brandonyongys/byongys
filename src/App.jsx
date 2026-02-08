04a4980ef2365fc23b7d6be55aaa4552bbff7d3e
Brandon Yong <brandon_yong_yung_sin@nuhs.edu.sg>
2026-02-08 21:38:12 +0800

    Implemented error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
+import { MarkdownProvider } from './context/MarkdownContext';
+import ErrorBoundary from './components/ErrorBoundary';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -13,33 +14,39 @@ import PostPage from './pages/PostPage';
 import Projects from './pages/Projects';
 import ProjectPage from './pages/ProjectPage'
 // import Tags from './pages/Tags';
-import SearchPage from './pages/SearchPage';  
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
+    <ErrorBoundary>
+      <MarkdownProvider>
+        <Router>
+          <ScrollToTop />
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
 
-        {/* Main content should grow to push Footer to the bottom */}
-        <main className="flex-grow text-orange-900">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/cv" element={<CV />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/posts/:slug" element={<PostPage />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/:slug" element={<ProjectPage />} />
-            {/* <Route path="/tags" element={<Tags />} /> */}
-            <Route path="/search" element={<SearchPage />} />
-          </Routes>
-        </main>
+            {/* Main content should grow to push Footer to the bottom */}
+            <main className="flex-grow text-orange-900">
+              <ErrorBoundary>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/cv" element={<CV />} />
+                  <Route path="/blog" element={<Blog />} />
+                  <Route path="/posts/:slug" element={<PostPage />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/projects/:slug" element={<ProjectPage />} />
+                  {/* <Route path="/tags" element={<Tags />} /> */}
+                  <Route path="/search" element={<SearchPage />} />
+                </Routes>
+              </ErrorBoundary>
+            </main>
 
-        <Footer />
-      </div>
-    </Router>
+            <Footer />
+          </div>
+        </Router>
+      </MarkdownProvider>
+    </ErrorBoundary>
   );
 }
 
