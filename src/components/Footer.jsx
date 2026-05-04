da86816923e34298068c3a8b811080457cc0374e
Brandon Yong <byongys@gmail.com>
2026-05-04 11:27:50 +0000

    Get current year outside the footer function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@
 // import the icon
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
-// Add the icon and link below
+const CURRENT_YEAR = new Date().getFullYear();
+
 export default function Footer() {
   return (
     <footer className="bg-brand-primary text-brand-text-main py-3 text-center opacity-60">
@@ -12,7 +13,7 @@ export default function Footer() {
         <a href="https://linkedin.com/in/byongys" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
         <a href="mailto:byongys@gmail.com"><FaEnvelope /></a>
       </div>
-      <p className="mt-2 text-sm opacity-100">&copy; {new Date().getFullYear()} Brandon Yong. All rights reserved.</p>
+      <p className="mt-2 text-sm opacity-100">&copy; {CURRENT_YEAR} Brandon Yong. All rights reserved.</p>
     </footer>
   );
 }
