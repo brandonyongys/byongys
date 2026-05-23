ddbcd037fbfca11431e34ffef1406dd6216e83ec
Brandon Yong <byongys@gmail.com>
2026-05-23 04:09:32 +0000

    feat: wire remark-directive and remarkFloatImage into markdown pipeline

--- src/config/markdownPlugins.js
+++ src/config/markdownPlugins.js
@@ -1,6 +1,8 @@
 import remarkGfm from 'remark-gfm';
+import remarkDirective from 'remark-directive';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
+import remarkFloatImage from '../utils/remarkFloatImage.js';
 
-export const REMARK_PLUGINS = [remarkGfm];
+export const REMARK_PLUGINS = [remarkGfm, remarkDirective, remarkFloatImage];
 export const REHYPE_PLUGINS = [rehypeRaw, rehypeSanitize];
