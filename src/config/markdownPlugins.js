de7214f76add9dc54ad71e2360a6f4979cdc487c
Brandon Yong <byongys@gmail.com>
2026-05-23 04:19:09 +0000

    fix: extend rehype-sanitize schema to allow figure/figcaption/class/style and add clearfix

--- src/config/markdownPlugins.js
+++ src/config/markdownPlugins.js
@@ -1,8 +1,20 @@
 import remarkGfm from 'remark-gfm';
 import remarkDirective from 'remark-directive';
 import rehypeRaw from 'rehype-raw';
-import rehypeSanitize from 'rehype-sanitize';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkFloatImage from '../utils/remarkFloatImage.js';
 
+const sanitizeSchema = {
+  ...defaultSchema,
+  tagNames: [...(defaultSchema.tagNames ?? []), 'figure', 'figcaption'],
+  attributes: {
+    ...defaultSchema.attributes,
+    figure: ['className', 'style'],
+    figcaption: [],
+    div: [...(defaultSchema.attributes?.div ?? []), 'className'],
+    img: [...(defaultSchema.attributes?.img ?? []), 'style'],
+  },
+};
+
 export const REMARK_PLUGINS = [remarkGfm, remarkDirective, remarkFloatImage];
-export const REHYPE_PLUGINS = [rehypeRaw, rehypeSanitize];
+export const REHYPE_PLUGINS = [rehypeRaw, [rehypeSanitize, sanitizeSchema]];
